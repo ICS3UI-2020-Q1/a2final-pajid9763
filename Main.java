4e946cde19b0b5156fd74bde8235b8a50c1b9fe4
pajid9763 <pajid9763@wrdsb.ca>
2020-09-15 15:48:42 +0000

    finished pizza pricing program

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * a program to calculate cost of pizza
+ * @author Darian Pajic
  */
 public class Main {
 
@@ -9,7 +10,40 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+  Scanner input = new Scanner(System.in);
+
+  // ask the user what size pizza they want
+  System.out.println("What is the size of pizza (in cm):");
+  double size = input.nextInt();
+
+  // assign variables
+  double diameter = size * 0.50;
+  double labour = 0.75;
+  double power = 0.99;
+
+  // find the price of the pizza
+  double sum = diameter + labour + power;
+  System.out.println("Subtotal: $" + sum);
+
+  // find the price of the tax
+  double tax = sum * 0.13;
+  System.out.println("Taxes: $" + tax);
+
+  // find total price of pizza
+  double total = sum + tax;
+  System.out.println("Total: $" + total);
+
+  // print a message to the customer
+  if (size >= 1 && size <= 20){
+    System.out.println("We are going to make you a cute little pizza!");
+  }else if (size > 20 && size <= 40){
+    System.out.println("This will be delicious!");
+  }else if (size > 40){
+    System.out.println("Whoa, big pizza! You might need a truck to get this home!");
+  }
+
+
+
     
   }
 }
